235e6feb4f3cee95d736188656f06d821d1f97e2
paulirish <paul.irish@gmail.com>
2010-04-13 18:34:54 -0400

    new color

--- etherpad/src/etherpad/globals.js
+++ etherpad/src/etherpad/globals.js
@@ -20,7 +20,7 @@
 
 var COMETPATH = "/comet";
 
-var COLOR_PALETTE = ['#ffc6c6','#ffe2bf','#fffcbf','#cbffb3','#b3fff1','#c6e7ff','#dcccff','#ffd9fb'];
+var COLOR_PALETTE = ['#ffc6c6','#ffe2bf','#fffcbf','#cbffb3','#b3fff1','#c6e7ff','#dcccff','#ffd9fb', '#bada55'];
 
 function isProduction() {
   return (appjet.config['etherpad.isProduction'] == "true");
